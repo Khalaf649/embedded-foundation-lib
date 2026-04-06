145e26566d449f7db941f8c9ed9e82dd17a1c763
Khalaf <abdlrhmankhalaf566@gmail.com>
2026-04-06 18:25:16 +0200

    implement the Decoder and RegisterHandler

--- include/Message.h
+++ include/Message.h
@@ -24,15 +24,19 @@ typedef union {
     uint8  quarter[4];
 } Payload_t;
 
-/* The Main Message Struct (Exactly 6 Bytes) */
+/* The Main Message Struct*/
 typedef struct {
-    Payload_t payload; /* Bytes 2-5: Fixed 4-byte data field */
     uint8     header;  /* Byte 0: Bits 2:0 = Type, Bit 3 = Prio, Bits 7:4 = Seq */
     uint8     length;  /* Byte 1: Always set to 4 */
+    Payload_t payload; /* Bytes 2-5: Fixed 4-byte data field */
 } __attribute__((packed)) Message_t;
+
+typedef void (*MsgHandler_t)(Message_t *DecodedMsg);
 
 Std_ReturnType Msg_Encode(uint8 Type, uint8 Priority, Payload_t *InPayload, P_void OutBuffer);
 Std_ReturnType PreProcess_SensorReading(uint16 Temperature, uint16 Humidity, Payload_t *OutPayload);
 Std_ReturnType PreProcess_DeviceCommand(uint8 CommandID, uint8 Parameter, Payload_t *OutPayload);
+Std_ReturnType Msg_RegisterHandler(uint8 MsgType, MsgHandler_t Handler);
+Std_ReturnType Msg_Decode(P_void InBuffer, Message_t *OutMsg);
 
 #endif //EMBEDDED_PROJECT1_MESSAGE_H
--- src/Message.c
+++ src/Message.c
@@ -8,6 +8,7 @@
 
 
 static uint8 Msg_SeqNum = 0;
+static MsgHandler_t HandlerTable[8] = {NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL};
 /* Preprocessor for Sensor Reading Data */
 Std_ReturnType PreProcess_SensorReading(uint16 Temperature, uint16 Humidity, Payload_t *OutPayload) {
     if (OutPayload == NULL) {
@@ -54,4 +55,42 @@ Std_ReturnType Msg_Encode(uint8 Type, uint8 Priority, Payload_t *InPayload, P_vo
 
     return E_OK;
 
+}
+
+Std_ReturnType Msg_RegisterHandler(uint8 MsgType, MsgHandler_t Handler) {
+    /* Message type must be 0-7. */
+    if (MsgType > 7) {
+        return E_NOT_OK;
+    }
+
+    /* Store the function pointer in our encapsulated array */
+    HandlerTable[MsgType] = Handler;
+
+    return E_OK;
+}
+
+Std_ReturnType Msg_Decode(P_void InBuffer, Message_t *OutMsg) {
+
+    if (InBuffer == NULL || OutMsg == NULL) {
+        return E_NOT_OK;
+    }
+
+    uint8 *RawBytes = (uint8 *)InBuffer;
+
+    OutMsg->header = RawBytes[0];
+    OutMsg->length = RawBytes[1];
+
+    OutMsg->payload.quarter[0] = RawBytes[2];
+    OutMsg->payload.quarter[1] = RawBytes[3];
+    OutMsg->payload.quarter[2] = RawBytes[4];
+    OutMsg->payload.quarter[3] = RawBytes[5];
+
+    uint8 MsgType = (OutMsg->header & 0x07);
+
+
+    if (HandlerTable[MsgType] != NULL) {
+        HandlerTable[MsgType](OutMsg);
+    }
+
+    return E_OK;
 }
