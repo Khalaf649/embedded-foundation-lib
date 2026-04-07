2888ac4b396cc9c05d4d1177fe178318cc6761cd
Khalaf <abdlrhmankhalaf566@gmail.com>
2026-04-07 22:17:27 +0200

    test: finalize MemScanner diagnostic suite and formatted CLI output

--- src/main.c
+++ src/main.c
@@ -2,6 +2,7 @@
 #include "STD_TYPES.h"
 #include "MemScanner.h"
 #include "Message.h"
+#include "BIT_MATH.h"
 
 /* Sensor Data Handler */
 void SensorData_Handler(Message_t *Msg) {
@@ -20,8 +21,9 @@ void Command_Handler(Message_t *Msg) {
 }
 int main(void) {
     uint8 SensorBuffer[6], CommandBuffer[6];
-    Message_t DummyResult;
+    Message_t DummyResult; // Used in Message Decoding
     Payload_t TempPayload;
+    Payload_t SensorPayload;
 
     /* Register Handlers */
     Msg_RegisterHandler(MSG_TYPE_SENSOR_READING, SensorData_Handler);
@@ -32,8 +34,8 @@ int main(void) {
     Msg_Encode(MSG_TYPE_SENSOR_READING, MSG_PRIORITY_NORMAL, &TempPayload, SensorBuffer);
 
     /*  Encode Command (ID 0x01, Param 0xFF) */
-    PreProcess_DeviceCommand(0x01, 0xFF, &TempPayload);
-    Msg_Encode(MSG_TYPE_DEVICE_COMMAND, MSG_PRIORITY_HIGH, &TempPayload, CommandBuffer);
+    PreProcess_DeviceCommand(0x01, 0xFF, &SensorPayload);
+    Msg_Encode(MSG_TYPE_DEVICE_COMMAND, MSG_PRIORITY_HIGH, &SensorPayload, CommandBuffer);
 
     /*  Decode & Trigger Handlers  */
     printf("Processing Sensor Data...");
@@ -41,6 +43,77 @@ int main(void) {
 
     printf("Processing Device Command...");
     Msg_Decode(CommandBuffer, &DummyResult);
+
+    printf("---------------------------------------------------------\n");
+   /*Raw Buffer for Hex Dumps*/
+    printf("Sensor Buffer  : ");
+    MemScan_HexDump(SensorBuffer, 6);
+    printf("Command Buffer : ");
+    MemScan_HexDump(CommandBuffer, 6);
+    printf("---------------------------------------------------------\n");
+
+    /* Read the header and the  length for the buffer*/
+    uint8 sensorHeader, deviceHeader, sensorLength, deviceLength;
+    MemScan_ReadByte(SensorBuffer, 0, &sensorHeader);
+    MemScan_ReadByte(CommandBuffer, 0, &deviceHeader);
+    MemScan_ReadByte(SensorBuffer, 1, &sensorLength);
+    MemScan_ReadByte(CommandBuffer, 1, &deviceLength);
+    printf("Sensor  -> Header: 0x%02X, Length: %u\n", sensorHeader, sensorLength);
+    printf("Command -> Header: 0x%02X, Length: %u\n", deviceHeader, deviceLength);
+    printf("---------------------------------------------------------\n");
+
+    /* Read Word and halfWord*/
+    uint16 temp;
+    uint32 word;
+    MemScan_ReadHalfWord(SensorBuffer, 2, &temp);
+    MemScan_ReadWord(SensorBuffer, 2, &word);
+    printf("HalfWord (Index 2) : %u\n", temp);
+    printf("Word     (Index 2) : 0x%08X\n", word);
+    printf("---------------------------------------------------------\n");
+
+   /*Buffer Comparsion with MemScan_HexDump */
+    uint8 copyBuffer[6];
+    for (uint8 i = 0; i < 6; i++) {
+        copyBuffer[i] = SensorBuffer[i];
+    }
+    uint8 updatedHeader = copyBuffer[0];
+    WRITE_BIT_FIELD(updatedHeader, 0x01, 3, 1, MSG_PRIORITY_HIGH);
+    MemScan_WriteByte(copyBuffer, 0, updatedHeader);
+
+    printf("Original Buffer : ");
+    MemScan_HexDump(SensorBuffer, 6);
+    printf("Modified Buffer : ");
+    MemScan_HexDump(copyBuffer, 6);
+    printf("---------------------------------------------------------\n");
+
+    /*Memory filling*/
+
+    uint8 emptyBuffer[6];
+    MemScan_Fill(emptyBuffer, 6, 0xAA);
+    printf("Filled Buffer   : ");
+    MemScan_HexDump(emptyBuffer, 6);
+    printf("---------------------------------------------------------\n");
+
+    /* Memory Compare with MemScan_Compare */
+    uint8 buffer1[6], buffer2[6];
+    for (uint8 i = 0; i < 6; i++) {
+        buffer1[i] = buffer2[i] = SensorBuffer[i];
+    }
+    uint32 offset1, offset2;
+    MemScan_Compare(buffer1, buffer2, 6, &offset1);
+
+    MemScan_WriteByte(buffer1, 1, 0xAA); /* Inject change */
+    MemScan_Compare(buffer1, buffer2, 6, &offset2);
+
+    printf("Offset 1 (Identical) : %u\n", offset1);
+    printf("Offset 2 (Modified)  : %u\n", offset2);
+    printf("---------------------------------------------------------\n");
+
+
+
+
+
+
 
     return 0;
 }
