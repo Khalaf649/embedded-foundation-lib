807392392b69888895676f5cd6b787f3311f300e
Khalaf <abdlrhman.mohamed02@eng-st.cu.edu.eg>
2026-05-13 00:27:13 +0300

    Clean up for new project

--- System/System.h
+++ System/System.h
@@ -4,16 +4,10 @@
 
 #ifndef SYSTEM_H
 #define SYSTEM_H
-#include "../Lcd/Lcd.h"
-#include "../Adc/Adc.h"
-#include "../Led/Led.h"
-#include "../Fan/Fan.h"
 
 
 
-extern Lcd_Config_t app_lcd;
-extern Led_Config_t alarm_led_cfg;
-extern Fan_Config_t cooling_fan_cfg;
+
 
 void System_InitAll(void);
 
